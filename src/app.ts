eb47776fc99d9d35f3bee8436ac8d20f803b4fcf
NurMuhammadCSE <nurmuhammadcse@gmail.com>
2023-06-17 23:38:23 +0600

    handle not found and logger

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 //app.ts
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 const app: Application = express();
 import cors from 'cors';
+import httpStatus from 'http-status';
 
 app.use(cors());
 
@@ -11,6 +12,21 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
+});
+
+// Handle Not Found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessage: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  });
+  next();
 });
 
 export default app;
